e6db977be4881479959ee4b64ab31517d0af395f
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-02-15 22:25:07 -0700

    [feature] add getSummary() and getRequestsCount() to campaign contract and redeploy

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -84,4 +84,20 @@ contract Campaign {
     request.recipient.transfer(request.value);
     request.complete = true;
   }
+
+  function getSummary() public view returns (
+    uint, uint, uint, uint, address
+  ) {
+    return (
+      minimumContribution,
+      this.balance,
+      requests.length,
+      contributorsCount,
+      owner
+    );
+  }
+
+  function getRequestsCount() public view returns (uint) {
+    return requests.length;
+  }
 }
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const contractInstance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x4b19D3c0d5651aC9483AA8aD2448a6C13ab2B3d4',
+  '0xf0A577BB46183cEE9a0f0a6591b661CE809D6F03',
 );
 
 export default contractInstance;
