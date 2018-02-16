853e7bcc521f78bd5c4ed4d31cd79478237882f2
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-02-15 17:22:30 -0700

    [feature] start new campaign submission set up

---
+++ pages/campaigns/new.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Form, Button, Input } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
+
+class CampaignNew extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { minimumContribution: '' };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  async onSubmit(e) {
+    e.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    await factory.methods
+      .createCampaign(this.state.minimumContribution)
+      .send({ from: accounts[0] });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h3>Create a New Campaign</h3>
+        <Form onSubmit={this.onSubmit}>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={e => this.setState({ minimumContribution: e.target.value })}
+            />
+          </Form.Field>
+          <Button primary>Create!</Button>
+        </Form>
+      </Layout>
+    )
+  }
+}
+
+export default CampaignNew;
